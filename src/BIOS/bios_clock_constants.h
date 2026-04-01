86b77957dce703b4cd51f5b485d1f005ea21995b
Hellorld! <jeremy.thornton@nhs.net>
2026-04-01 12:34:41 +0100

    Update bios_clock_constants.h

--- src/BIOS/bios_clock_constants.h
+++ src/BIOS/bios_clock_constants.h
@@ -3,7 +3,9 @@
  * @copyright   2024 Jeremy Simon Thornton
  * @version     0.1.0
  *
- * clock updates at 1193180/65536 (about 18.2) ticks per second.
+ * The IBM PC timer (8253/8254 PIT) runs at 1.193180 MHz.
+ * Channel 0 divides this by 65536 to generate IRQ 0 (INT 08h):
+ * clock updates at 1193180/65536 = 18.20649803161621... Hz ticks per second.
  * counts per second ? 18
  * counts per minute ? 1092
  * counts per hour   ? 65543
@@ -12,11 +14,15 @@
 #ifndef BIOS_CLOCK_CONSTANTS_H
 #define BIOS_CLOCK_CONSTANTS_H
 
-#define BIOS_CLOCK_SERVICES			1Ah
+#define BIOS_CLOCK_SERVICES			    1Ah
 #define BIOS_READ_SYSTEM_CLOCK_COUNTER	0
 #define BIOS_SET_SYSTEM_CLOCK_COUNTER	1
 
-#define TICKS_PER_SECOND			18.206
-#define TICKS_PER_24HR				1800B0h
+#define BIOS_PIT_FREQUENCY              1.193180 // MHz
+// Tick Period    = 65536 / 1193180 = 0.054925471... seconds (~54.925 ms)
+#define BIOS_CLOCK_PERIOD               0.054925493219799
+// Tick Frequency = 1193180 / 65536 = 18.20649803161621... Hz
+#define BIOS_TICKS_PER_SECOND			18.20649803161621
+#define BIOS_TICKS_PER_24HR				1800B0h
 
 #endif
